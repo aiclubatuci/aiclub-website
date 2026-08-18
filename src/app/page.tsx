23724724047253cbaa459ea2faf67c332d9e00b2
Johnson Nguyen <jhonsonn@uci.edu>
2026-08-18 13:17:21 -0700

    chore: added meta data to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
 import HomeSection from "@/components/sections/HomeSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AI at UCI - Home",
+  description:
+    "AI at UCI is a student-run organization at the University of California, Irvine that aims to promote the use of AI in the campus community.",
+  icons: {
+    icon: "/img/global/ailogo.png",
+  },
+};
 
 export default function Home() {
   return <HomeSection />;
